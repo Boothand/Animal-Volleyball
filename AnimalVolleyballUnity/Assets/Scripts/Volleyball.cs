ddfca48ed2d4fe207c4c30cb05dced59989b9854
Boothand <boothand@hotmail.com>
2016-06-19 21:12:59 +0200

    Testing ball physics

---
+++ AnimalVolleyballUnity/Assets/Scripts/Volleyball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Volleyball : MonoBehaviour
+{
+	Rigidbody rb;
+	[SerializeField] Vector3 startDirection;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+	}
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			rb.AddForce(startDirection);
+		}
+
+	}
+}
