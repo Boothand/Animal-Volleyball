b99b717b6db00fcce21798fe73abe80469c49a6f
Boothand <boothand@hotmail.com>
2016-06-23 00:19:36 +0200

    Not really that useful button for resetting cam pos

---
+++ AnimalVolleyballUnity/Assets/Editor/CameraEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerCamera))]
+public class CameraEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		PlayerCamera cam = (PlayerCamera)target;
+
+		if (GUILayout.Button("Move to target position"))
+		{
+			cam.transform.position = cam.CameraPosObject.position;
+			cam.transform.forward = cam.CameraPosObject.forward;
+		}
+	}
+}
