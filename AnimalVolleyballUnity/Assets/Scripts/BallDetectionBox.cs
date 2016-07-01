8084ad377640513a71c47aced8bd9ff568a8e6ed
Boothand <boothand@hotmail.com>
2016-07-01 22:54:41 +0200

    For detecting if the ball is within reach for a certain type of hit

---
+++ AnimalVolleyballUnity/Assets/Scripts/BallDetectionBox.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BallDetectionBox : MonoBehaviour
+{
+	//Private
+	Volleyball ball;
+	[SerializeField] bool ballInBox;
+
+	//Public
+	public Volleyball Ball { get { return ball; } }
+	public bool BallInBox { get { return ballInBox; } }
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.GetComponent<Volleyball>())
+		{
+			ball = col.GetComponent<Volleyball>();
+			ballInBox = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.GetComponent<Volleyball>())
+		{
+			ball = null;
+			ballInBox = false;
+		}
+	}
+}
